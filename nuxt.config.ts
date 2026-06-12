00a1105659862508135dc543df429d977db7415b
joao <joao.arruda.corp@gmail.com>
2026-06-12 19:44:20 -0300

    EASYIA site

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-01-01',
   devtools: { enabled: false },
+  nitro: {
+    preset: 'node-server'
+  },
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/css/tailwind.css'],
   tailwindcss: {
