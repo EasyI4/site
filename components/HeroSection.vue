60c6e7ac6a115b838f3e5b46c4b76aaeb1ec45f5
João Arruda <joao.arruda.corp@gmail.com>
2026-09-23 16:28:02 -0300

    Sistemas Agenticos

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -33,7 +33,7 @@ const ctaHref =
         </h1>
 
         <p class="mt-7 max-w-2xl text-lg leading-relaxed text-muted" v-reveal="120">
-          Automatizamos processos, conectamos dados e construímos Plataformas Agênticas.
+          Automatizamos processos, conectamos dados e desenvolvemos Sistemas Agênticos.
           Menos retrabalho. Mais escala.
         </p>
 
